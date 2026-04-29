2f674e8b4c7bb7585276076fca71af40387813c3
IulianPeta <iulian.petru.peta@gmail.com>
2026-04-29 12:30:16 +0300

    add Dev class using IoC from spring

--- demo/src/main/java/com/spring/demo/DemoApplication.java
+++ demo/src/main/java/com/spring/demo/DemoApplication.java
@@ -2,15 +2,16 @@ package com.spring.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+    public static void main(String[] args) {
 
+        ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
 
-		Dev object = new Dev();
-		object.build();
-	}
+        Dev object = context.getBean(Dev.class);
+        object.build();
+    }
 }
